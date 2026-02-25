be3f997a3ef4270dfe27c4e6e26b523788a18b1c
idusha38-alt <idusha38@gmail.com>
2026-02-25 22:41:13 +0530

    Update protocols.py

--- protocols.py
+++ protocols.py
@@ -2,6 +2,7 @@ import threading
 import pygame
 from gpiozero import LED
 from time import sleep
+import paho.mqtt.client as mqtt
 
 pygame.mixer.init()
 
@@ -25,7 +26,7 @@ def start_protocol(animal_name):
             light.blink(0.2, 0.2, n=20) 
         
         else:
-            print(“unknown animal”)
+            print("unknown animal")
             return
 
         try:
@@ -34,8 +35,23 @@ def start_protocol(animal_name):
             
             while pygame.mixer.music.get_busy() or light.is_lit:
                 sleep(0.1)
+        except:
+            pass
                 
-
 
     logic_thread = threading.Thread(target=run)
     logic_thread.start()
+
+
+def on_connect(client, userdata, flags, rc):
+    client.subscribe("animal/detection")
+
+def on_message(client, userdata, msg):
+    start_protocol(msg.payload.decode().strip())
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect(""broker.emqx.io", 1883, 60")
+client.loop_forever()
