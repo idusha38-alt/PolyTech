9057d68a63b5908127ac5f3ec5a111a2dfcca2a3
idusha38-alt <idusha38@gmail.com>
2026-03-03 21:38:14 +0530

    Update protocols.py

--- protocols.py
+++ protocols.py
@@ -4,54 +4,47 @@ from gpiozero import LED
 from time import sleep
 import paho.mqtt.client as mqtt
 
-pygame.mixer.init()
+client = mqtt.Client()
+// pygame.mixer.init()
 
 
-light = LED() 
 
 
-def start_protocol(animal_name):
+def start_protocol(animal_name): 
+    if animal_name == "deer":
+        // sound_file = ""
+        print("Deer detected")
+    elif animal_name == "elephant":
+        // sound_file = ""
+        print("Elephant detected")
+
+    elif animal_name == "wild_boar":
+       //  sound_file = ""
+        print("Wild boardetected")
     
-    def run():
+    else:
+        print("unknown animal")
+        return
+
+    try:
+        pygame.mixer.music.load()
+        pygame.mixer.music.play()
         
-        if animal_name == "deer":
-            sound_file = ""
-            light.blink(0.2, 0.2, n=15) 
-        elif animal_name == "elephant":
-            sound_file = ""
-            light.blink(0.05, 0.05, n=50) 
-
-        elif animal_name == "wild_boar":
-            sound_file = ""
-            light.blink(0.2, 0.2, n=20) 
-        
-        else:
-            print("unknown animal")
-            return
-
-        try:
-            pygame.mixer.music.load()
-            pygame.mixer.music.play()
-            
-            while pygame.mixer.music.get_busy() or light.is_lit:
-                sleep(0.1)
-        except:
-            pass
-                
-
-    logic_thread = threading.Thread(target=run)
-    logic_thread.start()
-
+        // while pygame.mixer.music.get_busy() or light.is_lit:
+            sleep(0.1)
+    except:
+        pass
+    display_message = f"ALARM ACTIVE: {animal_name.upper()} DETECTED!"
+    client.publish("alerts/display", display_message) 
 
 def on_connect(client, userdata, flags, rc):
-    client.subscribe("animal/detection")
+    client.subscribe("yolo/data")
 
 def on_message(client, userdata, msg):
     start_protocol(msg.payload.decode().strip())
 
-client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect(""broker.emqx.io", 1883, 60")
+client.connect("broker.emqx.io", 1883, 60)
 client.loop_forever()
